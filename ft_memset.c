106f0e6fea5f3c263732bade90354cc242bcb609
Sabata MOFOKENG <samofoke@c4r4s1.wethinkcode.co.za>
2019-05-27 14:22:23 +0200

    phase2

--- ft_memset.c
+++ ft_memset.c
@@ -6,8 +6,24 @@
 /*   By: samofoke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 14:58:21 by samofoke          #+#    #+#             */
-/*   Updated: 2019/05/22 14:58:33 by samofoke         ###   ########.fr       */
+/*   Updated: 2019/05/27 14:21:16 by samofoke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	*ft_memset(void *b, int c, size_t n)
+{
+	unsigned char *x;
+	unsigned char y;
+	int index;
+
+	x = (unsigned char*)b;
+	y = (unsigned char)c;
+	index = 0;
+	while (n--)
+	{
+		x[index++] = y;
+	}
+	return x;
+}
