3d3a68105840ef32484cfb933b0aab062dd0790c
Sabata MOFOKENG <samofoke@c4r4s1.wethinkcode.co.za>
2019-05-28 14:56:02 +0200

    phase1

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,8 +6,25 @@
 /*   By: samofoke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 15:01:40 by samofoke          #+#    #+#             */
-/*   Updated: 2019/05/22 15:01:45 by samofoke         ###   ########.fr       */
+/*   Updated: 2019/05/28 14:54:23 by samofoke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t index;
+	unsigned char *d;
+	unsigned char *s;
+
+	index = 0;
+	d = (unsigned char*)dst;
+	s = (unsigned char*)src;
+	while (index < n)
+	{
+		d[index] = s[index];
+		index++;
+	}
+	return (d);
+}
