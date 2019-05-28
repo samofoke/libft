09f36a9bd3429613e0689697d68f58a0402101c2
Sabata MOFOKENG <samofoke@c4r4s1.wethinkcode.co.za>
2019-05-28 14:57:08 +0200

    3 functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: samofoke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 14:13:57 by samofoke          #+#    #+#             */
-/*   Updated: 2019/05/22 14:21:24 by samofoke         ###   ########.fr       */
+/*   Updated: 2019/05/28 14:54:33 by samofoke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,5 @@
 # include <stdio.h>
 
 void	*ft_memset(void *b, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	ft_memcpy(void *dst, const void *src, size_t n);
