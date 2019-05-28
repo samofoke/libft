438fcda187da5f58862779ad8ccf3f711ffa7406
Sabata MOFOKENG <samofoke@c4r4s1.wethinkcode.co.za>
2019-05-28 11:13:31 +0200

    phase1

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,8 +6,13 @@
 /*   By: samofoke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 14:59:41 by samofoke          #+#    #+#             */
-/*   Updated: 2019/05/22 14:59:45 by samofoke         ###   ########.fr       */
+/*   Updated: 2019/05/28 11:12:20 by samofoke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_bzero(void *b, size_t n)
+{
+	ft_memset(b, 0, n);
+}
