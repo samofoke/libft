64c63322e503a87b4e8e3e6fab97fc893aaa4061
Sabata MOFOKENG <samofoke@c4r4s1.wethinkcode.co.za>
2019-05-29 10:01:53 +0200

    new adds functions and M/Libft

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,8 +6,32 @@
 /*   By: samofoke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 15:01:59 by samofoke          #+#    #+#             */
-/*   Updated: 2019/05/22 15:02:04 by samofoke         ###   ########.fr       */
+/*   Updated: 2019/05/29 09:46:02 by samofoke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	unsigned char x;
+	unsigned char *d;
+	unsigned char *s;
+	size_t  index;
+
+	x = (unsigned char)c;
+	d = (unsigned char*)dst;
+	s = (unsigned char*)src;
+	index = 0;
+	while (index++ < n)
+	{
+		if (*s == x)
+		{
+			*d = x;
+			d++;
+			return (d);
+		}
+		*d++ = *s++;
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: samofoke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 14:13:57 by samofoke          #+#    #+#             */
-/*   Updated: 2019/05/28 14:54:33 by samofoke         ###   ########.fr       */
+/*   Updated: 2019/05/29 09:57:33 by samofoke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,8 @@
 
 void	*ft_memset(void *b, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
-void	ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void	*ft_memmove(void *dst, const void *src, int c, size_t n);
+void	*ft_memchr(const void *b, int c, size_t n);
+int		memcmp(const void *s1, const void *s2, size_t n);
