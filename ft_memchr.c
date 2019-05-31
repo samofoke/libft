7a2ef75a943562ab2d10aa5ef68fd8a2e793b4f0
Sabata MOFOKENG <samofoke@c4r4s1.wethinkcode.co.za>
2019-05-31 12:59:27 +0200

    small fixes

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,8 +6,48 @@
 /*   By: samofoke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 08:33:18 by samofoke          #+#    #+#             */
-/*   Updated: 2019/05/23 08:33:23 by samofoke         ###   ########.fr       */
+/*   Updated: 2019/05/31 12:57:55 by samofoke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char *st;
+	unsigned char *x;
+
+	st = (unsigned char*)s;
+	x = (unsigned char*)c;
+	while (n--)
+	{
+		if (st == x)
+		{
+			return (st);
+		}
+		st++;
+	}
+	return (NULL);
+}
+
+int		main(void)
+{
+	char b[80];
+	char *ptr;
+
+	strcpy(b , "We are immortals");
+	printf("%s\n", b);
+	ptr = (char *) memchr(b, 'i', 10);
+	if (ptr == NULL)
+	{
+		printf("The char is not found in the string\n");
+	}
+	else
+	{
+		printf("%s\n", ptr);
+	}
+	return 0;
+}
--- libft.h
+++ libft.h
@@ -6,14 +6,14 @@
 /*   By: samofoke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 14:13:57 by samofoke          #+#    #+#             */
-/*   Updated: 2019/05/29 10:36:21 by samofoke         ###   ########.fr       */
+/*   Updated: 2019/05/31 10:04:42 by samofoke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LIBFT_H
 # define FT_LIBFT_H
 # include <unistd.h>
-# include <stdio.h>
+# include <string.h>
 # include <stdlib.h>
 
 void	*ft_memset(void *b, int c, size_t n);
@@ -22,6 +22,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, int c, size_t n);
 void	*ft_memchr(const void *b, int c, size_t n);
-int		memcmp(const void *s1, const void *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
