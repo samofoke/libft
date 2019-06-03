133438292eb4b983a09d18b3521b4ef977589ced
samofoke <samofoke@student.wethinkcode.co.za>
2019-06-03 22:58:30 +0200

    strcat not fixed

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: samofoke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 14:13:57 by samofoke          #+#    #+#             */
-/*   Updated: 2019/06/02 22:06:31 by slimy            ###   ########.fr       */
+/*   Updated: 2019/06/03 20:53:41 by slimy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,12 @@ void	*ft_memchr(const void *b, int c, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 char	ft_strdup(const char *src);
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char *dst, const char *src, size_t n);
+char	*ft_strcat(char *dst, const char *src);
+char	*ft_strncat(char *dst, const char *src, size_t n);
+size_t	ft_strlcat(char *dst, const char *src, size_t n);
+char	*strchr(const char *s, int c);
+char	*strrchr(const char *s, int c);
 
 #endif
