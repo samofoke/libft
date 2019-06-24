e779e302086ae811dcdc903a189f4bfc3a1fb50f
Sabata MOFOKENG <samofoke@c4r8s10.wethinkcode.co.za>
2019-06-24 17:29:34 +0200

    done

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: samofoke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 09:59:10 by samofoke          #+#    #+#             */
-/*   Updated: 2019/06/13 15:24:46 by samofoke         ###   ########.fr       */
+/*   Updated: 2019/06/24 16:54:52 by samofoke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,17 @@ int		ft_strcmp(const char *s1, const char *s2)
 {
 	unsigned char	*x1;
 	unsigned char	*x2;
+	int				x;
 
+	x = 0;
 	x1 = (unsigned char*)s1;
 	x2 = (unsigned char*)s2;
-	while (*x1 && *x2)
-	{
-		if ((*x1 > *x2) || (*x1 < *x2))
-			return (*x1 - *x2);
-		x1++;
-		x2++;
-	}
-	return (*x1 - *x2);
+	while (x1[x] && x2[x] && x1[x] == x2[x])
+		x++;
+	if ((x1[x] - x2[x]) < 0)
+		return (-1);
+	else if ((x1[x] - x2[x]) > 0)
+		return (1);
+	else
+		return (0);
 }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: samofoke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 09:59:28 by samofoke          #+#    #+#             */
-/*   Updated: 2019/06/17 10:19:20 by samofoke         ###   ########.fr       */
+/*   Updated: 2019/06/24 17:28:28 by samofoke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,27 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned char *x1;
-	unsigned char *x2;
+	unsigned char	*x1;
+	unsigned char	*x2;
+	size_t			x;
 
+	x = 0;
 	x1 = (unsigned char*)s1;
 	x2 = (unsigned char*)s2;
-	if (n <= 0)
-		return (0);
-	if (!ft_strlen((char*)x1) || !ft_strlen((char*)x2))
-		return (*x1 - *x2);
-	while (n-- && *x1 && *x2)
+	while (n > 0)
 	{
-		if ((*x1 > *x2) || (*x1 < *x2))
-			return (*x1 - *x2);
-		if (n == 0)
+		if (!(x1[x] || x2[x]))
 			break ;
-		x1++;
-		x2++;
+		if (x1[x] != x2[x])
+		{
+			if ((x1[x] - x2[x]) < 0)
+				return (-1);
+			else if ((x1[x] - x2[x]) > 0)
+				return (1);
+		}
+		else
+			x++;
+		n--;
 	}
-	return (*x1 - *x2);
+	return (0);
 }
